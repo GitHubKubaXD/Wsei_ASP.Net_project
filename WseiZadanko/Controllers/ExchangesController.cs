2cf1ab8641c0efc261ae242df8706826085e6d44
GitHubKubaXD <GitHubKubaXD@.github.com>
2020-10-11 20:21:30 +0200

    Dodaj pliki projektów.

---
+++ WseiZadanko/Controllers/ExchangesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WseiZadanko.Models;
+
+namespace WseiZadanko.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ExchangeModel exchange)
+        {
+            var viewModel = new ExchangeAddedViewModel
+            {
+                NumberOfCharsInName = exchange.Name.Length,
+                NumberOfCharsInDescription = exchange.Description.Length,
+                IsHidden = !exchange.IsVisible
+            };
+
+            return View("ExchangeAdded", viewModel);
+        }
+    }
+}
---
+++ WseiZadanko/Models/ExchangeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WseiZadanko.Models
+{
+    public class ExchangeModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+
+    }
+}
